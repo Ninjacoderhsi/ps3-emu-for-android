043f0f2d0985ebf1840a038c39c6787eda32d916
ninjacoder <89408667+Ninjacoderhsi@users.noreply.github.com>
2021-11-06 15:51:48 +0330

    Update core.cpp

--- C++/Core/core.cpp
+++ C++/Core/core.cpp
@@ -1,1 +1,42 @@
 
+
+////all code by ninja coder soft by 2021 in app in beta use moblie 6 gp ram up and 256 gb rom tnk///
+
+#include <..res/mvk.mm>
+#include <string>
+#include <bool>
+
+#define "stdlib.h"
+#define "c.h"
+#define "code.mm"
+
+int ninjacoder app ()
+{
+  
+  
+  if << is string call static << (get.string(index.def)),
+  thread_local { [] std::array<c, N> 
+  [ def , incastel , path , ]
+  showmassges(do * 0 * zip);
+  
+}
+{
+  try {
+    void `Filename('', 'ClassName')`::memberFunction() {
+      int 0 ;
+       if( for ( alignof == && 0 && 11 ))
+       {
+         volatile 0 / 0 ,
+         string path dirt , local hosting get dier path , 
+         
+       }
+       else
+       {
+         bool 0 ;
+         return get.std::list<array + liststring> ;
+       }
+    }
+  }
+}
+
+#endif
